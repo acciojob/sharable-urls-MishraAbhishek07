e29a0683c444c711bce5685c0a5c148e6156fbd1
MishraAbhishek07 <davmishra96@gmail.com>
2024-09-08 02:21:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ let button=document.getElementById("button");
 
 button.addEventListener("click",function(){
    let name = encodeURIComponent(nameInput.value.trim());  
-   let year = encodeURIComponent(yearInput.value.trim());
+   let year = encodeURIComponent(yearInput.value.trim();
 
  if(nameInput.value==""&&yearInput.value==""){
 	document.getElementById("ur1").innerHTML=`https://localhost:8080/?`
    
  } else if(nameInput.value!==""&&yearInput.value==''){document.getElementById("ur1").innerHTML=`https://localhost:8080/?name=${name}`
 
-}  else if(yearInput.value!==""&&nameInput.value==''){document.getElementById("ur1").innerHTML=`https://localhost:8080/?name=year=${year}`}
+}  else if(yearInput.value!==""&&nameInput.value==''){document.getElementById("ur1").innerHTML=`https://localhost:8080/?year=${year}`}
 else if(yearInput.value!==""&&nameInput.value!==''){document.getElementById("ur1").innerHTML=`https://localhost:8080/?name=${name}&year=${year}`}
 
 
