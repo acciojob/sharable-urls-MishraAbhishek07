9b77e38da095f2ff8537bf4f763180cf8ae773e6
MishraAbhishek07 <davmishra96@gmail.com>
2024-09-08 02:32:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ button.addEventListener("click",function(){
    let year = encodeURIComponent(yearInput.value.trim());
 
  if(nameInput.value==""&&yearInput.value==""){
-	document.getElementById("ur1").innerHTML=`https://localhost:8080/?`
+	document.getElementById("url").innerHTML=`https://localhost:8080/?`
    
- } else if(nameInput.value!==""&&yearInput.value==''){document.getElementById("ur1").innerHTML=`https://localhost:8080/?name=${name}`
+ } else if(nameInput.value!==""&&yearInput.value==''){document.getElementById("url").innerHTML=`https://localhost:8080/?name=${name}`
 
-}  else if(yearInput.value!==""&&nameInput.value==''){document.getElementById("ur1").innerHTML=`https://localhost:8080/?year=${year}`}
-else if(yearInput.value!==""&&nameInput.value!==''){document.getElementById("ur1").innerHTML=`https://localhost:8080/?name=${name}&year=${year}`}
+}  else if(yearInput.value!==""&&nameInput.value==''){document.getElementById("url").innerHTML=`https://localhost:8080/?year=${year}`}
+else if(yearInput.value!==""&&nameInput.value!==''){document.getElementById("url").innerHTML=`https://localhost:8080/?name=${name}&year=${year}`}
 
 
 
