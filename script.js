e1bfeaa30534bb7ee0212103ee992f22cb95a543
MishraAbhishek07 <davmishra96@gmail.com>
2024-09-08 01:55:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let nameInput=document.getElementById("name");
 let yearInput=document.getElementById("year");
 let button=document.getElementById("button");
- const urlElement = document.getElementById('url');
+ const urlElement = document.getElementById('url1');
 
 
 
@@ -10,11 +10,15 @@ button.addEventListener("click",function(){
    let name = encodeURIComponent(nameInput.value.trim());  
    let year = encodeURIComponent(yearInput.value.trim());
 
-  let queryString = 'https://localhost:8080/?';
-        if (name) queryString += `name=${name}`;
-        if (name && year) queryString += '&';
-        if (year) queryString += `year=${year}`;
-	urlElement.textContent = queryString;
+ if(name.value==""&&year.value==""){
+	document.getElementById("ur1").innerHTML=`https://localhost:8080/?`
+   
+ } else if(name.value!==""&&year.value==''){document.getElementById("ur1").innerHTML=`https://localhost:8080/?name=${name}`
+
+}  else if(year.value!==""&&name.value==''){document.getElementById("ur1").innerHTML=`https://localhost:8080/?name=year=${year}`}
+else if(year.value!==""&&name.value!==''){document.getElementById("ur1").innerHTML=`https://localhost:8080/?name=${name}&year=${year}`}
+
+
 
 })
 
