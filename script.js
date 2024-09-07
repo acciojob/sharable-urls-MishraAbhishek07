2061ce5b5981cd74d007ad069b5aaae4ab997ba1
MishraAbhishek07 <davmishra96@gmail.com>
2024-09-08 00:40:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 // your code here
-name=document.getElementById("name")
-year=document.getElementById("year")
-button=document.getElementById("button")
+let name=document.getElementById("name");
+let year=document.getElementById("year");
+let button=document.getElementById("button");
+
+
+button.addEventListener("click",function(){
+   if(name.value==""&&year.value==""){
+	document.getElementById("ur1").innerHTML=`https://localhost:8080/?`
+   
+ } else if(name.value!==""&&year.value==''){document.getElementById("ur1").innerHTML=`https://localhost:8080/?name=${name.value}`
+
+}  else if(year.value!==""&&name.value==''){document.getElementById("ur1").innerHTML=`https://localhost:8080/?name=year=${year.value}`}
+else if(year.value!==""&&name.value!==''){document.getElementById("ur1").innerHTML=`https://localhost:8080/?name=${name.value}&year=${year.value}`}
+
+
+})
+
+   
+	
